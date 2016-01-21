7df537ca42734ad6b77b6e22c568aac5ed45c9bd
aclaesson <alexander.claesson83@gmail.com>
2016-01-21 12:44:16 +0100

    first committ

---
+++ src/main/java/jee7fiddle/PersonResource.java
@@ -0,0 +1,66 @@
+package jee7fiddle;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@ApplicationPath("/resources")
+@Path("persons")
+public class PersonResource extends Application {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private Integer countPersons() {
+        Query query = entityManager.createQuery("SELECT COUNT(p.id) FROM Person p");
+        return ((Long) query.getSingleResult()).intValue();
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Person> findPersons(int startPosition, int maxResults, String sortFields, String sortDirections) {
+        Query query = entityManager.createQuery("SELECT p FROM Person p ORDER BY " + sortFields + " " + sortDirections);
+        query.setFirstResult(startPosition);
+        query.setMaxResults(maxResults);
+        return query.getResultList();
+    }
+
+    public PaginatedListWrapper<Person> findPersons(PaginatedListWrapper<Person> wrapper) {
+        wrapper.setTotalResults(countPersons());
+        int start = (wrapper.getCurrentPage() - 1) * wrapper.getPageSize();
+        wrapper.setList(findPersons(start,
+                                    wrapper.getPageSize(),
+                                    wrapper.getSortFields(),
+                                    wrapper.getSortDirections()));
+        return wrapper;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public PaginatedListWrapper<Person> listPersons(@DefaultValue("1")
+                                                    @QueryParam("page")
+                                                    Integer page,
+                                                    @DefaultValue("id")
+                                                    @QueryParam("sortFields")
+                                                    String sortFields,
+                                                    @DefaultValue("asc")
+                                                    @QueryParam("sortDirections")
+                                                    String sortDirections) {
+        PaginatedListWrapper<Person> paginatedListWrapper = new PaginatedListWrapper<>();
+        paginatedListWrapper.setCurrentPage(page);
+        paginatedListWrapper.setSortFields(sortFields);
+        paginatedListWrapper.setSortDirections(sortDirections);
+        paginatedListWrapper.setPageSize(5);
+        return findPersons(paginatedListWrapper);
+    }
+}
